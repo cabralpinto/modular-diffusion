e1366b75177d65a0e250c491787dd9d863884c0a
cabralpinto <jmcabralpinto@gmail.com>
2023-08-29 16:10:52 +0100

    Docs: Add sitemap for SEO

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,4 +1,5 @@
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
@@ -7,7 +8,7 @@ import remarkLayout from "./src/plugins/remark-layout.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     remarkPlugins: [remarkMath, remarkLayout],
     rehypePlugins: [rehypeKatex],
